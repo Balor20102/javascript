73e8d85b534fdc91586a69f5cb06df383d72d973
DanielKool <daniel@familiekool.nl>
2022-01-31 08:43:33 +0100

    week3

---
+++ week3/kennisvragen/test.js
@@ -0,0 +1,10 @@
+const dayOfTheWeek = "Monday";
+switch(dayOfTheWeek){
+    case "Monday":
+    case "Thursday":
+        console.log("konijn!");
+    case "Tuesday": 
+        console.log("Koe!");
+    default:
+        console.log("vos!");
+}
